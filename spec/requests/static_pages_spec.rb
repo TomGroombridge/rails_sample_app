ac872da9f779ae8587d99ad5dad20f8d960d8db0
TomGroombridge <groombridgetom@gmail.com>
2014-01-15 11:37:11 +0000

    user page layed out

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,56 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'tom groombridge'" do
-      visit root_path
-      expect(page).to have_content('Tom Groombridge')
+      it { should have_content('Tom Groombridge') }
+      it { should have_title('Home')}
     end
-  
-
- 		it "should have the right title" do 
- 			visit root_path		
- 			expect(page).to have_title('Home')
- 		end
-	end
+	
 
   describe "Help Page" do
+    before { visit help_path }
 
-  	it "should have the content 'help'" do
-  		visit help_path
-  		expect(page).to have_content('Help')
+  		it { should have_content('Help')}
+      it { should have_title('Help')}
   	end
-
- 		it "should have the right title" do 
- 			visit help_path		
- 			expect(page).to have_title('Help')
- 		end
- 	end
+ 	
 
   describe "About page" do 
-
-  	it "should have the content 'About us'" do
-  		visit about_path
-  		expect(page).to have_content("About")
-  	end
-
-  	 it "should have the right title" do 
- 			visit about_path		
- 			expect(page).to have_title('About')
+    before { visit about_path }
+    it { should have_content('About')}
+    it { should have_title('About')}
  		end
- 	end
+ 	
 
  	describe "Contact page" do
-
- 		it "should have the right title" do
- 			visit contact_path
- 			expect(page).to have_title('Contact')
- 		end
-
- 		it "should have the content 'Contact'" do
- 			visit contact_path
- 			expect(page).to have_content('Contact')
- 		end
+    before { visit contact_path}
+    it {should have_title('Contact')}
+    it {should have_content('Contact')}
  	end
 end
